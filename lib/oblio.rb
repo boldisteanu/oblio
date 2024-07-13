5cf4aaecfdcf516fc476e10028484a698a62c401
Cristian Boldisteanu <cristian@boldisteanu.ro>
2024-07-13 23:27:40 +0300

    Update oblio.rb

--- lib/oblio.rb
+++ lib/oblio.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
-
-require_relative "oblio/version"
+require 'net/http'
+require 'json'
+require_relative 'oblio/version'
 
 module Oblio
   class Error < StandardError; end
-  # Your code goes here...
+
 end
