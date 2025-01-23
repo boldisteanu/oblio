64052712110304366d00778ad1e2ed1106fa7b02
Cristian Boldisteanu <cristian@boldisteanu.ro>
2025-01-23 19:52:17 +0200

    Update oblio.rb

--- lib/oblio.rb
+++ lib/oblio.rb
@@ -2,6 +2,9 @@
 require 'net/http'
 require 'json'
 require_relative 'oblio/version'
+require_relative 'oblio/api'
+require_relative 'oblio/invoice'
+require_relative 'oblio/company'
 
 module Oblio
   class Error < StandardError; end
